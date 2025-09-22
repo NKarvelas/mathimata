857ee9b445e795d34fb73b797df8de0b81cd539e
DESKTOP-M61MKFL\arkan <nikos.karvelas.gd@gmail.com>
2025-09-22 03:43:07 +0300

    test3

---
+++ Mathima/Taksi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathima
+{
+    public class Taksi
+    {
+        int arithmos1 = 0;
+        int arithmos2 = 0;
+
+        public int prosthesi(int ar1, int ar2)
+        {
+            int sum = ar1 + ar2;
+
+            return sum;
+        }
+    }
+}
